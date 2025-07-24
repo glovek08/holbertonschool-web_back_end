7e07b8ef184c20ac966df96c5fa8868fc0cfb91f
glovek08 <gbrielbbarn@gmail.com>
2025-07-24 19:15:39 -0300

    feat: return list of tuples

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+
+"""
+from typing import List, Tuple, Sized
+
+
+def element_length(lst: List[Sized]) -> List[Tuple[Sized, int]]:
+    """
+    Takes a list of iterable elements and returns a list of tuples,
+    each containing the element and its length.
+    Args:
+        lst (List[Iterable]): A list of iterable elements (e.g.
+          strings, lists, etc.).
+    Returns:
+        List[Tuple[Iterable, int]]: A list of tuples where each tuple contains
+        the element from the input list and its length.
+    """
+    return [(i, len(i)) for i in lst]
