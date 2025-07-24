4412763c0ad33a2cd329748fa30bf469244f9ac9
glovek08 <gbrielbbarn@gmail.com>
2025-07-24 17:19:08 -0300

    feat: move import below module docstring

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from math import floor as floor_this
-
 """
 This module contains a type annotated function,
 Aliased floor cause name conflic with function name.
 """
+
+from math import floor as floor_this
 
 
 def floor(n: float) -> int:
