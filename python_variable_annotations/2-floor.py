768fd02da4340792a424c9c9cdbaa205a7886b1d
glovek08 <gbrielbbarn@gmail.com>
2025-07-24 17:16:06 -0300

    feat: floor float to int

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from math import floor as floor_this
+
+"""
+This module contains a type annotated function.
+"""
+
+
+def floor(n: float) -> int:
+    """
+    This function takes a float n as argument and
+    returns the floor of the float.
+
+    Args:
+        n (float): float to be floored.
+
+    Returns:
+        int: n floored.
+    """
+    return floor_this(n)
+
+
+print(floor(12.52))
