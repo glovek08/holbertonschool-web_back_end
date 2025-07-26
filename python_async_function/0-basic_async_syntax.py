110b5bb9ddbc955c5a5882c735a36b150bff53aa
glovek08 <gbrielbbarn@gmail.com>
2025-07-25 21:48:07 -0300

    fix: return type annotation

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -7,7 +7,7 @@ import asyncio
 from random import uniform
 
 
-async def wait_random(max_delay: int = 10) -> int:
+async def wait_random(max_delay: int = 10) -> float:
     """
     Takes in an integer argument (max_delay, with a default value of 10)
     waits for a random delay between 0 and max_delay (included and float value)
