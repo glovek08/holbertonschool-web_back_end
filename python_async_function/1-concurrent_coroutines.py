23a7a8227ebf6bf4ac1b5f741ab62e06bb8930f8
glovek08 <gbrielbbarn@gmail.com>
2025-07-25 22:26:06 -0300

    feat: who eats icecream the fastest

---
+++ python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+"""
+SPAM SPAM SPAM SPAM SPAM WITH CHICKEN SOUP AND SPAM.
+"""
+wait_random = __import__("0-basic_async_syntax").wait_random
+from sys import stdout
+from typing import List
+import asyncio
+
+
+async def wait_n(n: int, max_delay: int = 10) -> List[float]:
+    """
+    Creates a list of routines, dispatches them for execution concurrently,
+    and returns the list of delays in the order they complete (ascending).
+
+    Args:
+        n (int): the number of routines.
+        max_delay (int, optional): max seconds for await. Defaults to 10.
+
+    Returns:
+        List[float]: A list of delays in the order they completed.
+    """
+    try:
+        icecream = [wait_random(max_delay) for spam in range(n)]
+        fastest_eater = []
+        for kid_eats in asyncio.as_completed(icecream):
+            delay = await kid_eats
+            fastest_eater.append(delay)
+        return fastest_eater
+    except ValueError as v_error:
+        stdout.write(str(v_error) + "\n")
+        return []
