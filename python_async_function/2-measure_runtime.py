6941ed045f0e862cb409c02d1992bd28b48fc4be
glovek08 <gbrielbbarn@gmail.com>
2025-07-26 13:18:49 -0300

    chore: module docstring at top-level

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-import asyncio
-import time
-
 """
 Defines a measure_time function for measuring the
 runtime of specified coroutines.
 """
+import asyncio
+import time
 
 wait_n = __import__("1-concurrent_coroutines").wait_n
 
