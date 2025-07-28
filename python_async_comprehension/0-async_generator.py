5c4e4ea3075a4cf4b64dc0d0279d2b71b0a852d6
glovek08 <gbrielbbarn@gmail.com>
2025-07-28 15:00:53 -0300

    feat: async generator with delay

---
+++ python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/use/bin/env python3
+"""
+Defines a coroutine that loops and has a little bit of delay.
+"""
+import random
+import asyncio
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Asynchronous generator that yields 10 random floats
+    between 0.0 and 10.0 inclusive, each after a 1-second delay.
+    """
+    for i in range(10):
+        await asyncio.sleep(1)
+        yield random.uniform(0, 10)
