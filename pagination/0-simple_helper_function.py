44a5a4d0315f51e14d2c39b18feadbaf724671ab
glovek08 <gbrielbbarn@gmail.com>
2025-08-11 22:34:29 -0300

    refactor: function checks file descriptor boundry, proper validation bro.

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -12,31 +12,36 @@ Keyword arguments:
 import csv
 
 
-def index_range(page: int = 0, page_size: int = 0):
+def index_range(
+    page: int = 0, page_size: int = 1, filename: str = "Popular_Baby_Names.csv"
+) -> tuple[int, int]:
     """
     Calculate the start and end indexes for pagination.
 
     Args:
         page (int): The current page number (1-indexed).
         page_size (int): The number of items per page.
+        filename (str, optional): CSV file to use. Defaults to 'Popular_Baby_Names.csv'.
 
     Returns:
         tuple: A tuple containing the start index and end
         index for the given page.
     """
-    index_at = 0
+    row_count = 0
     try:
-        my_file = open("Popular_Baby_Names.csv", "r")
+        my_file = open(filename, "r")
         my_csv = csv.reader(my_file)
         for row in my_csv:
-            index_at += 1
+            row_count += 1
         if page > 0 and page_size > 0:
             start = (page - 1) * page_size
+            end = start + page_size
+            if start >= row_count:
+                return (row_count, row_count)
+            if end > row_count:
+                end = row_count
         else:
             start = 0
-        if page > 0 and page_size > 0:
-            end = start + page_size
-        else:
             end = 0
         return (start, end)
     except OSError as error:
