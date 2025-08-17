1040d13d05e1904af8b2cb3b73c0386b45c8e2db
glovek08 <gbrielbbarn@gmail.com>
2025-08-17 19:54:22 -0300

    docs: detailes docstrings

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Mongo collection handling.
+Module that defines a fucking mongo collection listing function
 """
 import pymongo
 from pymongo.collection import Collection
@@ -9,5 +9,9 @@ from pymongo.collection import Collection
 def list_all(mongo_collection: Collection) -> list:
     """
     Returns mongo collection list.
+    Arg:
+        - mongo_collection: a PyMongo collection object.
+    Returns:
+        list: A list of dictionaries. Or an empty list if the collection is empty.
     """
     return list(mongo_collection.find())
