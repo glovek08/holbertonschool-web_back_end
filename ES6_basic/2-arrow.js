3948ee59f8a0ff3c7868362de8b61630d0896a94
glovek08 <gbrielbbarn@gmail.com>
2025-08-20 21:23:14 -0300

    fix: remove add function

--- ES6_basic/2-arrow.js
+++ ES6_basic/2-arrow.js
@@ -1,10 +1,7 @@
 export default function getNeighborhoodsList() {
   this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
-
-  const add = (newNeighborhood) => {
+  this.addNeighborhood = (newNeighborhood) => {
     this.sanFranciscoNeighborhoods.push(newNeighborhood);
-    return self.sanFranciscoNeighborhoods;
+    return this.sanFranciscoNeighborhoods;
   }
-
-  this.addNeighborhood = add;
 }
