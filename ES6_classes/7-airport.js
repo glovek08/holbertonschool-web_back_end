ba3400d975b30db87267c67dcf2be64fcbd9904a
glovek08 <gbrielbbarn@gmail.com>
2025-08-23 18:58:10 -0300

    feat: airport class

---
+++ ES6_classes/7-airport.js
@@ -0,0 +1,28 @@
+class Airport {
+    constructor(name, code) {
+        this.name = name;
+        this.code = code;
+    }
+    get name() {
+        return this._name;
+    }
+    set name(value) {
+        if (typeof value !== "string") {
+            throw new TypeError("Name must be a string");
+        }
+        this._name = value;
+    }
+    get code() {
+        return this._code;
+    }
+    set code(value) {
+        if (typeof value !== "string") {
+            throw new TypeError("Code must be a string");
+        }
+        this._code = value;
+    }
+    toString() {
+        //[object SFO]
+        return `[${typeof this} ${this.code}]`;
+    }
+}
