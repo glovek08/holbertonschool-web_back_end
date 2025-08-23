d98428bfc90e880d6688f88e6eae9d9d49d7ba62
glovek08 <gbrielbbarn@gmail.com>
2025-08-23 18:21:17 -0300

    feat: pricing class

---
+++ ES6_classes/4-pricing.js
@@ -0,0 +1,48 @@
+import Currency from './3-currency.js';
+
+class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('Value must be an instance of Currency');
+    }
+    this._currency = value;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    if (typeof conversionRate !== 'number') {
+      throw new TypeError('Conversion rate must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
+
+// const pri1 = new Pricing(69, new Currency("€", "Euro"));
+
+// console.log("TEST: " + pri1.displayFullPrice());
