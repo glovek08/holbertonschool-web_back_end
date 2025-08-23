fe77d89f6a0419a7e8ed2245071701ec1e37b654
glovek08 <gbrielbbarn@gmail.com>
2025-08-23 18:34:37 -0300

    feat: building abstract class

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+class Building {
+    constructor(sqft) {
+        this.sqft = sqft;
+    }
+    get sqft() {
+        return this._sqft;
+    }
+    set sqft(value) {
+        if (typeof value !== "number") {
+            throw new TypeError("SqFt must be a number");
+        }
+        this._sqft = value;
+    }
+    evacuationWarningMessage() {
+        throw new Error("Class extending Building must override evacuationWarningMessage");
+    }
+}
+export default Building;
