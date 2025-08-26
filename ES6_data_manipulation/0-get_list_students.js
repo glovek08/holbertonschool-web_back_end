777d892d38df669268a04fe21d2232b269290789
glovek08 <gbrielbbarn@gmail.com>
2025-08-26 19:04:18 -0300

    feat: get list of students

---
+++ ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,15 @@
+class Student {
+    constructor(id, firstName, location) {
+        this.id = id;
+        this.firstName = firstName;
+        this.location = location;
+    }
+}
+export const getListStudents = () => {
+    const students = []
+    students.push(new Student(1, "Guillaume", "San Francisco"));
+    students.push(new Student(2, "James", "Columbia"));
+    students.push(new Student(5, "Serena", "San Francisco"));
+    return students;
+}
+console.log(getListStudents());
