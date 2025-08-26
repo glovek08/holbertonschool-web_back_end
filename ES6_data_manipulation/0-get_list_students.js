c557716b8b6e19dc59b42b7bb0e4f022cd446811
glovek08 <gbrielbbarn@gmail.com>
2025-08-26 19:11:08 -0300

    fix: export default

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -5,11 +5,12 @@ class Student {
         this.location = location;
     }
 }
-export const getListStudents = () => {
+const getListStudents = () => {
     const students = []
     students.push(new Student(1, "Guillaume", "San Francisco"));
     students.push(new Student(2, "James", "Columbia"));
     students.push(new Student(5, "Serena", "San Francisco"));
     return students;
 }
-console.log(getListStudents());
+// console.log(getListStudents());
+export default getListStudents;
