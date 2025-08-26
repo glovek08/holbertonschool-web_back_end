1c599aad9b4b13067a2e0689649867f999692d4d
glovek08 <gbrielbbarn@gmail.com>
2025-08-26 20:17:45 -0300

    feat: students' id total

---
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -0,0 +1,8 @@
+const getStudentIdsSum = (students) => {
+    if (Array.isArray(students)) {
+        // let idSum = 0
+        // return students.map(el => { idSum += el.id; })
+        return students.reduce( (sum, students) => students.id + sum, 0);
+    } else return 0;
+}
+export default getStudentIdsSum;
