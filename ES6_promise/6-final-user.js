7d742d76dba8b1216b831f63a24c2b66df7e54a8
glovek08 <gbrielbbarn@gmail.com>
2025-08-30 23:17:29 -0300

    feat: resolve promises and return status with value

---
+++ ES6_promise/6-final-user.js
@@ -0,0 +1,36 @@
+// Import signUpUser from 4-user-promise.js and uploadPhoto from 5-photo-reject.js.
+// Write and export a function named handleProfileSignup.
+// It should accept three arguments firstName (string), lastName (string), and fileName (string).
+// The function should call the two other functions.
+// When the promises are all settled it should return an array with the following structure:
+// [
+//     {
+//       status: status_of_the_promise,
+//       value: value or error returned by the Promise
+//     },
+//     ...
+//   ]
+
+import signUpUser from "./4-user-promise.js";
+import uploadPhoto from "./5-photo-reject.js";
+
+export default async function handleProfileSignup(
+  firstName,
+  lastName,
+  fileName
+) {
+  const userPromise = signUpUser(firstName, lastName);
+  const photoPromise = uploadPhoto(fileName);
+  const results = (
+    await Promise.allSettled([userPromise, photoPromise])
+  ).map((responses) =>
+    responses.status === "fulfilled"
+      ? responses
+      : { status: responses.status, value: responses.reason }
+  );
+  return results;
+}
+
+// handleProfileSignup("Bob", "Dylan", "bob_dylan.jpg").then((results) =>
+//   console.log(results)
+// );
