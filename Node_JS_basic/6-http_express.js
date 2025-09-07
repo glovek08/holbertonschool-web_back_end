714f6bfcf06308fc30a4e3286e00e95503e4dbe3
Gabriel Barn <gbrielbbarn@gmail.com>
2025-09-07 12:42:54 -0300

    feat: simple express HTTP server

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,23 @@
+// Install Express and in a file named 6-http_express.js,
+// create a small HTTP server using Express module:
+//     It should be assigned to the variable app and this one must be exported
+//     HTTP server should listen on port 1245
+//     Displays Hello Holberton School! in the page body for the endpoint /
+
+const express = require('express');
+// const os = require('os');
+
+const app = express();
+const PORT = 1245;
+const HOST = 'localhost';
+
+app.get('/', (req, res) => {
+  res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, HOST, () => {
+  // console.log(`Server on: http://${HOST}:${PORT}`);
+  // console.log(`Host: ${os.hostname}`);
+});
+
+module.exports = app;
